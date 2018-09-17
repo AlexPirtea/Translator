57b5a0b499feabb28007e1f2585ee3b090e02041
Alex Pirtea <alexandru.pirtea@devpadawan.ro>
2018-09-17 18:56:40 +0300

    Add project files.

---
+++ Translator/Translator/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translator.ViewModel;
+using Xamarin.Forms;
+
+namespace Translator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = ((ViewModelLocator)Application.Current.Resources["Locator"]).Main;
+        }
+    }
+}
---
+++ Translator/Translator/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Translator.ViewModel
+{
+    class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        [SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+        
+    }
+}
